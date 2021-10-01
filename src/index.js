2fd3857fc394aff5d1a790fd6f98f44d83ea8d1f
Vishesh Singh <VisheshSingh16@gmail.com>
2021-09-30 21:57:48 -0500

    initial webpack setup with cli and dev server

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const ryu = {
+  name: 'Ryu - the red dragon',
+  age: 28,
+  belt: 'purple',
+};
+
+const chunLi = {
+  name: 'chun-li',
+  age: 17,
+  belt: 'yellow',
+};
+
+console.log(ryu, chunLi);
